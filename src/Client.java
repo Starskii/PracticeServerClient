557c12570a5b6ddb7768f5f1c09245c86c956155
Starskii <jakeylakie@yahoo.com>
2020-12-07 22:20:49 -0500

    Added the receive data portion of client

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.Duration;
 import java.time.Instant;
@@ -18,10 +19,14 @@ public class Client {
     public static void main(String[] args) throws IOException{
         int ID = generateID();
         System.out.println(ID);
-        Socket s = new Socket("localhost", 4999);
+        Socket sendingSocket = new Socket("localhost", 4999);
+        Socket receivingSocket;
         OutputStream os;
+        InputStream is;
         BufferedOutputStream bos;
+        BufferedInputStream bis;
         ObjectOutputStream oos;
+        ObjectInputStream ois;
 
         //Example Object
         Gamestate g = new Gamestate(ID, "Hello World!");
@@ -31,15 +36,34 @@ public class Client {
         String message = g.message;
         while(!message.equals("exit")){
             g.message = message;
-            os = s.getOutputStream();
+            os = sendingSocket.getOutputStream();
             bos = new BufferedOutputStream(os);
             oos = new ObjectOutputStream(bos);
             oos.writeObject(g);
 
             oos.flush();
 
+            //Get gamestate back from server
+            boolean listening = true;
+            while(listening){
+                ServerSocket clientListening = new ServerSocket(ID);
+                receivingSocket = clientListening.accept();
+                is = receivingSocket.getInputStream();
+                bis = new BufferedInputStream(is);
+                ois = new ObjectInputStream(bis);
+                try {
+                    g = (Gamestate) ois.readObject();
+                    listening = false;
+                } catch (ClassNotFoundException e) {
+                    System.out.println("Received incompatible Object Type from Server");
+                }
+                ois.close();
+                bis.close();
+                is.close();
+                System.out.println(g.message);
+            }
             message = clientInputGetter.next();
         }
-        s.close();
+        sendingSocket.close();
     }
 }
